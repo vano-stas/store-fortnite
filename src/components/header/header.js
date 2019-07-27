bb892dca28124c0de84708e38a229fe5f1e1384a
Vano-Stas <st.ivan727@gmail.com>
2019-07-27 17:29:10 +0300

    Change navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,15 +7,19 @@ function Header ({ counterAll }) {
     return (
         // <div className='header-app'>
         <nav className='navigation-app'>
-            <Link className='page-link-item' to='/'>
-                <h3 className='page-link-item'>Fortnite Store</h3>
+            <Link className='logo-item' to='/'>
+                <h3 className='logo-item'>Fortnite Store</h3>
             </Link>
             <ul className='nav-links-group'>
-                <Link className='page-link-item' to='/'>
-                    <li className='page-link-item' >Home</li>
+                <Link className='' to='/'>
+                    <li className='page-link-item' >
+                        <button className='btn btn btn-outline-warning button-item'>Home</button>
+                    </li>
                 </Link>
-                <Link className='page-link-item' to='/store'>
-                    <li className='page-link-item' >Catalog</li>
+                <Link className='' to='/store'>
+                <li className='page-link-item' >
+                        <button className='btn btn btn-outline-warning button-item'>Catalog</button>
+                    </li>
                 </Link>
             </ul>
             <button className='btn btn-outline-light'><i class="fa fa-shopping-cart cart-item"></i> ({counterAll}) </button>
